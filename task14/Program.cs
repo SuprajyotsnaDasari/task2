f0596d10f01ab6cb60acf2e755d85297e4e9a7de
dsjumbc <jyotsnachowdary308@gmail.com>
2025-03-26 15:07:58 -0700

    task13

---
+++ task14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace PrimeNum {
+    class Program {
+        static void Main(string[]args){
+
+            for (int i = 2; i < 51; i++) {  
+                bool isPrime = true;  
+
+                for (int j = 2; j < i; j++) {
+                    if (i % j == 0) { 
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime) {
+                    Console.WriteLine($"{i} is a Prime Number");
+                }
+            }
+        }
+    }
+}
