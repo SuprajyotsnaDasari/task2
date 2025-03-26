fe03cbb37c08dc7bf446cff0f84364a61d31e091
dsjumbc <jyotsnachowdary308@gmail.com>
2025-03-25 20:27:06 -0700

    task7

---
+++ task7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SeasonName{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Enter a number between 1-4");
+            string enteredNum = Console.ReadLine();
+
+            int num = Int32.Parse(enteredNum);
+
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Spring");
+                    break;
+                case 2:
+                    Console.WriteLine("Summer");
+                    break;
+                case 3:
+                    Console.WriteLine("Autumn");
+                    break;
+                case 4:
+                    Console.WriteLine("Winter");
+                    break;
+                default:
+                    Console.WriteLine("Wrong input");
+                    break;
+            }
+        }
+    }
+}
