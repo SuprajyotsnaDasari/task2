84d5c9fd3238ba8dbcf8f347139e43acfc0a84f7
dsjumbc <jyotsnachowdary308@gmail.com>
2025-03-26 11:57:43 -0700

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PositiveNum {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter a positive value");
+            
+
+            while (true)
+            {
+                string numEnter = Console.ReadLine();
+                int num = Int32.Parse(numEnter);
+
+                if (num >= 0)
+                {
+                    Console.WriteLine($"Entered value: {num}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Entered value is negative, please enter positive value.");
+                }
+
+            }
+        }
+    }
+}
