ad514706ca414fcd22c0e91da2d507e3bd386f9e
dsjumbc <jyotsnachowdary308@gmail.com>
2025-03-26 15:29:28 -0700

    task17

--- task17/Program.cs
+++ task17/Program.cs
@@ -1,1 +1,28 @@
-﻿Write a program that asks the user to enter a positive number and continues asking until the user enters a number greater than 100.
+﻿ //positive number and continues asking until the user enters a number greater than 100.
+
+
+ using System;
+
+ namespace PositiveNumber {
+    class Program {
+        static void Main(string[] args) {
+            int num;
+
+            do{
+                Console.WriteLine("Enter a number greater than 100");
+                string enterNum = Console.ReadLine();
+                num = Int32.Parse(enterNum);
+
+                if (num > 100)
+                {
+                    Console.WriteLine("The number you entered is greater than 100");
+                }
+            } while (num <= 100);
+            Console.WriteLine("Number entered is not greater than 100");
+
+        }
+
+    }
+ }
+    
+ 
