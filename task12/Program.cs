8cdc81b5cfc43b1b411da46ce3566b545d7b2800
dsjumbc <jyotsnachowdary308@gmail.com>
2025-03-26 14:53:34 -0700

    task12

---
+++ task12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MulCal {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter a number for multipling");
+            string enterNum = Console.ReadLine();
+            int num = Int32.Parse(enterNum);
+
+            for (int i = 1; i < 11; i++)
+            {
+                int result = num * i;
+                Console.WriteLine($"{num} * {i} = {result}");
+            }
+        }
+    }
+}
