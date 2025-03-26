a016c0b185990c263192fdcc1397b00f30fe74ab
dsjumbc <jyotsnachowdary308@gmail.com>
2025-03-25 19:50:10 -0700

    changes

---
+++ task5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LeapYear{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Enter a year:");
+            string yearEntered = Console.ReadLine();
+
+            int year = Int32.Parse(yearEntered);
+
+            if (year%4 == 0 && (year%100 != 0 || year/400 %= 0))
+                Console.WriteLine("Leap year");
+            else
+                Console.WriteLine("Not Leap Year");
+        }
+    }
+}
