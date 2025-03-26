4ee916c8e026d90bb76a3b6077c47cb75531d26e
dsjumbc <jyotsnachowdary308@gmail.com>
2025-03-26 14:59:10 -0700

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CalCulator {
+    class Program {
+        static void Main (string[] args) {
+            for (int i = 1; i < 101; i++)
+            {
+                int result = (i*(i+1))/2;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
