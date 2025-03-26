c5caaa5ebeda5f129a8ba249ddea57bd1f257b54
dsjumbc <jyotsnachowdary308@gmail.com>
2025-03-26 12:06:40 -0700

    task11

---
+++ task11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SpellWord {
+    class Program {
+        static void Main(string[] args) {
+            string word = "Fibonacci";
+            int wordLength = word.Length;
+            int i =0;
+            
+            while (i < wordLength)
+            {
+                Console.WriteLine(word[i]);
+                i++;
+            }
+        }
+    }
+}
