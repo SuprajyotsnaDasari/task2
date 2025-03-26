d705efd5c27a062d9a941606dcadfb80786f5241
dsjumbc <jyotsnachowdary308@gmail.com>
2025-03-26 11:43:14 -0700

    task9

---
+++ task9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OddNum {
+    class Program {
+        static void Main(string[] args) {
+            int n = 1;
+            
+            while ( n<21)
+            {
+                Console.WriteLine(n);
+                n+=2;
+            }
+        }
+    }
+}
