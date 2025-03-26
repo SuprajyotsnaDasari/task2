62d1dc28f20450d0e9a7f198fcfd9594de94ace4
dsjumbc <jyotsnachowdary308@gmail.com>
2025-03-25 20:38:36 -0700

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace orderedItem{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Enter a number between 1-5");
+            string enteredNum = Console.ReadLine();
+
+            int num =Int32.Parse(enteredNum);
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Coffee");
+                    break;
+                case 2:
+                    Console.WriteLine("Tea");
+                    break;
+                case 3:
+                    Console.WriteLine("Juice");
+                    break;
+                case 4:
+                    Console.WriteLine("Water");
+                    break;
+                case 5:
+                    Console.WriteLine("Soda");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+        }
+    }
+}
