b6ae10db5cde827066c8f4bccb12890309795927
dsjumbc <jyotsnachowdary308@gmail.com>
2025-03-25 19:32:06 -0700

    temp

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TempCal{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Enter the temperature:");
+            string temperature = Console.ReadLine();
+            int temp = Int32.Parse(temperature);
+
+            if (temp >= 30)
+                Console.WriteLine("Hot");
+            else if (temp > 15 && temp<=29)
+                Console.WriteLine("Warm");
+            else
+                Console.WriteLine("Cold");
+        }
+    }
+}
