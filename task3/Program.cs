e6f9162ad0bd3c71375a78799c3817adb9999538
dsjumbc <jyotsnachowdary308@gmail.com>
2025-03-25 19:26:32 -0700

    task3 marks

---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GradeEvaluator{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Enter the marks (0-100)?");
+            string marks = Console.ReadLine();
+            int mark = Int32.Parse(marks);
+
+            if (mark >= 50)
+            {
+                Console.WriteLine("Pass");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+        }
+    }
+}
